a6b88d56da3904c5fbac1a5caaae2d3f81f52157
Rositsa Koriykova <rosi.korikova@gmail.com>
2016-12-03 17:57:38 +0200

    contacts.js

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class AboutPage extends Component {
     render() {
         return (
-            <div>About Page</div>
+            <div></div>
         );
     }
 }
