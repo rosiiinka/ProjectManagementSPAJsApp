eb60c54f1c051cf9a61124d631b4cf991b881145
Rositsa Koriykova <rosi.korikova@gmail.com>
2016-12-03 19:23:37 +0200

    update everything

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class AboutPage extends Component {
     render() {
         return (
-            <div></div>
+            <div>About Page</div>
         );
     }
 }
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,21 +1,11 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router';
+import React, { Component } from 'react';
+
 
 export default class HomePage extends Component {
     render() {
-        let message = <p>You are currently not logged in. Please, log in or register to view team options.</p>;
-
-        if (sessionStorage.getItem('username')) {
-            if (sessionStorage.getItem('teamId')) {
-                message = <Link to={"/catalog/" + sessionStorage.getItem('teamId')}>Go to my team</Link>
-            } else {
-                message = <p>You are currently not a member of a team. View the <Link to="/catalog">catalog</Link> to join or create one.</p>;
-            }
-        }
         return (
-            <div>
-                <h1>Home Page</h1>
-                {message}
+            <div className="home-page">
+
             </div>
         );
     }
