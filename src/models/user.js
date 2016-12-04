40da96eecdf60fc3e1efcd0263377a7e938596e2
RoApostolova <zionbg@abv.bg>
2016-12-04 17:16:28 +0200

    deleted unused things

--- src/models/user.js
+++ src/models/user.js
@@ -60,30 +60,6 @@ function logout(callback) {
     }
 }
 
-function joinTeam(teamId, callback) {
-    let userData = {
-        username: sessionStorage.getItem('username'),
-        teamId: teamId
-    };
-    requester.update('user', sessionStorage.getItem('userId'), userData, 'kinvey')
-        .then((response) => {
-            saveSession(response);
-            observer.onSessionUpdate();
-            callback(true);
-        });
-}
 
-function leaveTeam(callback) {
-    let userData = {
-        username: sessionStorage.getItem('username'),
-        teamId: ''
-    };
-    requester.update('user', sessionStorage.getItem('userId'), userData, 'kinvey')
-        .then((response) => {
-            saveSession(response);
-            observer.onSessionUpdate();
-            callback(true);
-        });
-}
 
-export {login, register, logout, joinTeam, leaveTeam};
+export {login, register, logout};
